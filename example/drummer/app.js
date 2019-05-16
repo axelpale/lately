bceefccfffc94f9d32fed57a6c2f1a99cf86845a
Akseli Palen <akseli.palen@gmail.com>
2019-05-16 14:16:10 +0300

    Interactive grid

--- example/drummer/app.js
+++ example/drummer/app.js
@@ -12,23 +12,101 @@ const createFrameElem = (t) => {
   return f;
 };
 
-const createCellElem = (t, ch) => {
+const createCellElem = (t, ch, value, numChannels) => {
   const cel = document.createElement('div');
   cel.dataset.time = t;
   cel.dataset.channel = ch;
   cel.classList.add('cell');
+
+  if (value === 1) {
+    cel.classList.add('cell-on');
+  }
+
+  cel.classList.add('cell-ch' + numChannels);
+
   return cel;
 };
 
-{
-  const timeline = document.getElementById('timeline');
+const bindCellElem = (cel, dispatch) => {
+  cel.addEventListener('click', () => {
+    dispatch({
+      type: 'SET_VALUE',
+      time: cel.dataset.time,
+      channel: cel.dataset.channel,
+      value: cel.classList.contains('cell-on') ? 0 : 1
+    });
+  });
+};
 
-  let t, ch, fr;
-  for (t = 0; t < 6; t += 1) {
+const clearElem = (el) => {
+  while(el.firstChild){
+    el.removeChild(el.firstChild);
+  }
+  return el;
+};
+
+const createTimelineElem = (hist, dispatch) => {
+  const timeline = document.createElement('div');
+
+  const channels = hist.length;
+  const duration = hist[0].length;
+
+  let t, ch, cel, val, fr;
+  for (t = 0; t < duration; t += 1) {
     fr = createFrameElem(t);
-    for (ch = 0; ch < 4; ch += 1) {
-      fr.appendChild(createCellElem(t, ch));
+    for (ch = 0; ch < channels; ch += 1) {
+      val = hist[ch][t];
+      cel = createCellElem(t, ch, val, channels);
+      bindCellElem(cel, dispatch)
+      fr.appendChild(cel);
     }
     timeline.appendChild(fr);
   }
+
+  return timeline;
+};
+
+const historySet = (hist, t, ch, value) => {
+  const newHist = hist.map((ch) => {
+    return ch.slice()
+  })
+  newHist[ch][t] = value;
+  return newHist;
 }
+
+{
+  const initialModel = {
+    history: [
+      [1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0], // sun up
+      [0,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0,1,1,1,0,0] // flower open
+    ]
+  };
+  let currentModel = initialModel;
+
+  const reducer = (model, ev) => {
+    switch (ev.type) {
+      case 'SET_VALUE':
+        return Object.assign({}, model, {
+          history: historySet(model.history, ev.time, ev.channel, ev.value)
+        })
+      default:
+        return model;
+    }
+  };
+
+  const render = (model) => {
+    const timeline = createTimelineElem(model.history, dispatch);
+    const timelineContainer = document.getElementById('timeline');
+    clearElem(timelineContainer);
+    timelineContainer.appendChild(timeline);
+  };
+
+  const dispatch = (ev) => {
+    const newModel = reducer(currentModel, ev);
+    render(newModel);
+    currentModel = newModel;
+  };
+
+  // Init
+  render(initialModel);
+};
