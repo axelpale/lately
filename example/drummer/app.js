0e520d5af96dd28b28476b5ed89529654dfce346
Akseli Palen <akseli.palen@gmail.com>
2019-05-21 01:18:50 +0300

    Display infogain for each channel

--- example/drummer/app.js
+++ example/drummer/app.js
@@ -278,8 +278,11 @@ const createAPriori = (model, dispatch) => {
   const container = document.createElement('div');
   const numChannels = model.history.length;
   const channelMeans = way.mean(model.history);
-
+  const channelGain = way.map(channelMeans, mcbsp.gain);
   const t = 0;
+
+  // A priori
+
   let fr = createFrameElem('a priori');
   let cr = createCellRowElem(t);
 
@@ -290,6 +293,21 @@ const createAPriori = (model, dispatch) => {
     val = channelMeans[c][t];
     cel = createCellElem(t, c, val, numChannels);
     cel.classList.add('cell-apriori');
+    cr.appendChild(cel);
+  }
+
+  fr.appendChild(cr);
+  container.appendChild(fr);
+
+  // Infogain
+
+  fr = createFrameElem('infogain');
+  cr = createCellRowElem(t);
+
+  for (c = 0; c < numChannels; c += 1) {
+    val = channelGain[c][t];
+    cel = createCellElem(t, c, val, numChannels);
+    cel.classList.add('cell-infogain');
     cr.appendChild(cel);
   }
 
