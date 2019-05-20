13f7dc8bf4667de43d3935017c0510e3e7abf3b4
Akseli Palen <akseli.palen@gmail.com>
2019-05-20 17:21:20 +0300

    Module senseway

---
+++ example/drummer/local_modules/senseway/index.js
@@ -0,0 +1,97 @@
+// Senseway, a multi-channel timeline.
+//
+
+exports.add = (wayA, wayB) => {
+  return wayA.map((ch, c) => ch.map((q, t) => q + wayB[c][t]))
+}
+
+exports.channel = (way, c) => {
+  // Pick one channel for a way.
+  return [way[c]]
+}
+
+exports.channels = (way, cs) => {
+  // Pick multiple channels
+  return cs.map(c => way[c])
+}
+
+exports.create = (width, len, fill) => {
+  if (typeof fill !== 'number') {
+    throw new Error('Fill must be number');
+  }
+
+  const channels = [];
+  let i, j, ch;
+  for (i = 0; i < width; i += 1) {
+    ch = [];
+    for (j = 0; j < len; j += 1) {
+      ch.push(fill);
+    }
+    channels.push(ch);
+  }
+  return channels;
+};
+
+exports.equal = (wayA, wayB) => {
+  let i, j, ch;
+  for (i = 0; i < wayA.length; i += 1) {
+    for (j = 0; j < wayA[0].length; j += 1) {
+      if (wayA[i][j] !== wayB[i][j]) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+exports.first = (way, n) => {
+  // First n frames; the oldest
+  return way.map(ch => ch.slice(0, n))
+}
+
+exports.frame = (way, t) => {
+  return way.map(ch => [ch[t]]) // frame is an array of arrays
+}
+
+exports.increase = (way, addition) => {
+  return way.map(ch => ch.map(quantum => quantum + addition))
+}
+
+exports.join = (wayA, wayB) => {
+  if (wayA.length !== wayB.length) {
+    throw new Error('Number of channels must match.')
+  }
+  return wayA.map((ch, c) => [].concat(ch, wayB[c]))
+}
+
+exports.last = (way, n) => {
+  // Last n frames; the most recent
+  return way.map(ch => ch.slice(ch.length - n, ch.length))
+}
+
+exports.len = (way) => {
+  return way[0].length
+}
+
+exports.map = (way, fn) => {
+  return way.map(ch => ch.map(quantum => fn(quantum)))
+}
+
+exports.mix = (wayA, wayB) => {
+  if (wayA[0].length !== wayB[0].length) {
+    throw new Error('Length of channels must match.')
+  }
+  return [].concat(wayA, wayB)
+}
+
+exports.multiply = (wayA, wayB) => {
+  return wayA.map((ch, c) => ch.map((q, t) => q * wayB[c][t]))
+}
+
+exports.scale = (way, multiplier) => {
+  return way.map(ch => ch.map(quantum => quantum * multiplier))
+}
+
+exports.slice = (way, begin, end) => {
+  return way.map(ch => ch.slice(begin, end))
+}
---
+++ example/drummer/local_modules/senseway/test.js
@@ -0,0 +1,139 @@
+// Testing
+
+const way = require('./index')
+const test = require('tape')
+
+const W0 = [
+  [1, 0, 1],
+  [0, 1, 1]
+]
+const W1 = [
+  [0, 0, 1],
+  [1, 0, 0]
+]
+const ONES = [
+  [1, 1, 1],
+  [1, 1, 1]
+]
+const ZEROS = [
+  [0, 0, 0],
+  [0, 0, 0]
+]
+
+test('add', (t) => {
+  t.deepEqual(way.add(W0, W1), [
+    [1, 0, 2],
+    [1, 1, 1]
+  ])
+  t.end()
+})
+
+test('channel', (t) => {
+  t.deepEqual(way.channel(W0, 1), [
+    [0, 1, 1]
+  ])
+  t.end()
+})
+
+test('channels', (t) => {
+  t.deepEqual(way.channels(W0, [0, 1]), [
+    [1, 0, 1],
+    [0, 1, 1]
+  ])
+  t.end()
+})
+
+test('create', (t) => {
+  t.deepEqual(way.create(2, 3, 1), ONES)
+  t.end()
+})
+
+test('equal', (t) => {
+  t.true(way.equal(W0, W0))
+  t.false(way.equal(W0, W1))
+  t.end()
+})
+
+test('first', (t) => {
+  t.deepEqual(way.first(W0, 2), [
+    [1, 0],
+    [0, 1]
+  ])
+  t.end()
+})
+
+test('frame', (t) => {
+  t.deepEqual(way.frame(W0, 1), [
+    [0],
+    [1]
+  ])
+  t.end()
+})
+
+test('increase', (t) => {
+  t.deepEqual(way.increase(ONES, 1), [
+    [2, 2, 2],
+    [2, 2, 2]
+  ])
+  t.end()
+})
+
+test('join', (t) => {
+  t.deepEqual(way.join(ZEROS, ONES), [
+    [0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 1, 1, 1]
+  ])
+  t.end()
+})
+
+test('last', (t) => {
+  t.deepEqual(way.last(W0, 2), [
+    [0, 1],
+    [1, 1]
+  ])
+  t.end()
+})
+
+test('len', (t) => {
+  t.equal(way.len(W0), 3)
+  t.end()
+})
+
+test('map', (t) => {
+  t.deepEqual(way.map(ONES, q => q * 2), [
+    [2, 2, 2],
+    [2, 2, 2]
+  ])
+  t.end()
+})
+
+test('mix', (t) => {
+  t.deepEqual(way.mix(ZEROS, ONES), [
+    [0, 0, 0],
+    [0, 0, 0],
+    [1, 1, 1],
+    [1, 1, 1]
+  ])
+  t.end()
+})
+
+test('multiply', (t) => {
+  t.deepEqual(way.multiply(ONES, ZEROS), ZEROS)
+  t.end()
+})
+
+test('scale', (t) => {
+  t.deepEqual(way.scale(ONES, 2), [
+    [2, 2, 2],
+    [2, 2, 2]
+  ])
+  t.end()
+})
+
+test('slice', (t) => {
+  t.deepEqual(way.slice(W0, 1, 3), [
+    [0, 1],
+    [1, 1]
+  ])
+  t.end()
+})
