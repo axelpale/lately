2f136543716bafe40f55990d3f51a9106b0b191d
Akseli Palen <akseli.palen@gmail.com>
2019-05-22 23:21:48 +0300

    Use renderWay to render apriori and predicted future

--- example/drummer/src/createAPrioriElem.js
+++ example/drummer/src/createAPrioriElem.js
@@ -1,48 +1,14 @@
 const way = require('senseway');
 const mcbsp = require('mcbsp');
-const createFrameElem = require('./createFrameElem');
-const createCellRowElem = require('./createCellRowElem');
-const createCellElem = require('./createCellElem');
+const renderWay = require('./renderWay');
 
 module.exports = (model, dispatch) => {
   const container = document.createElement('div');
-  const numChannels = model.history.length;
   const channelMeans = way.mean(model.history);
   const channelGain = way.map(channelMeans, mcbsp.gain);
-  const t = 0;
 
-  // A priori
-
-  let fr = createFrameElem('a priori');
-  let cr = createCellRowElem(t);
-
-  fr.classList.add('apriori');
-
-  let c, cel, val;
-  for (c = 0; c < numChannels; c += 1) {
-    val = channelMeans[c][t];
-    cel = createCellElem(t, c, val, numChannels);
-    cel.classList.add('cell-apriori');
-    cr.appendChild(cel);
-  }
-
-  fr.appendChild(cr);
-  container.appendChild(fr);
-
-  // Infogain
-
-  fr = createFrameElem('infogain');
-  cr = createCellRowElem(t);
-
-  for (c = 0; c < numChannels; c += 1) {
-    val = channelGain[c][t];
-    cel = createCellElem(t, c, val, numChannels);
-    cel.classList.add('cell-infogain');
-    cr.appendChild(cel);
-  }
-
-  fr.appendChild(cr);
-  container.appendChild(fr);
+  container.appendChild(renderWay(channelMeans, 'a priori'));
+  container.appendChild(renderWay(channelGain, 'infogain'));
 
   return container;
 };
--- example/drummer/src/createPredictedFutureElem.js
+++ example/drummer/src/createPredictedFutureElem.js
@@ -1,33 +1,10 @@
 const createFrameElem = require('./createFrameElem');
 const createCellRowElem = require('./createCellRowElem');
 const createCellElem = require('./createCellElem');
+const renderWay = require('./renderWay');
 const predict = require('./predict');
 
 module.exports = (model, dispatch) => {
-  const timeline = document.createElement('div');
-  const distance = model.predictionDistance;
-  const channels = model.history.length;
-  const currentTime = model.history[0].length;
-
   const prediction = predict(model);
-
-  let t, ch, cel, val, fr, cr;
-  for (t = 0; t < distance; t += 1) {
-    fr = createFrameElem(currentTime + t);
-    cr = createCellRowElem(currentTime + t);
-
-    fr.classList.add('prediction');
-
-    for (ch = 0; ch < channels; ch += 1) {
-      val = prediction[ch][t];
-      cel = createCellElem(t, ch, val, channels);
-      cel.classList.add('cell-prediction');
-      cr.appendChild(cel);
-    }
-    fr.appendChild(cr);
-
-    timeline.appendChild(fr);
-  }
-
-  return timeline;
+  return renderWay(prediction, 'prediction');
 };
