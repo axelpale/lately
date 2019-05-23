a77f54037769d10746aed9dbd4f9d0a2751b4444
Akseli Palen <akseli.palen@gmail.com>
2019-05-24 01:33:09 +0300

    style distance controls

--- example/drummer/src/createContextDistanceControl.js
+++ example/drummer/src/createContextDistanceControl.js
@@ -1,27 +1,16 @@
+const renderSlider = require('./renderSlider');
+
 module.exports = (model, dispatch) => {
-  const control = document.createElement('div');
-
-  const input = document.createElement('input');
-  input.type = 'range';
-  input.value = model.contextDistance;
-  input.name = 'contextDistance';
-  input.min = '1';
-  input.max = '16';
-  input.step = '1';
-
-  input.addEventListener('change', (ev) => {
-    dispatch({
-      type: 'SET_CONTEXT_DISTANCE',
-      value: parseInt(input.value)
-    });
+  return renderSlider({
+    class: 'context-distance',
+    name: 'contextDistance',
+    value: model.contextDistance,
+    label: 'context length',
+    change: (sliderValue) => {
+      dispatch({
+        type: 'SET_CONTEXT_DISTANCE',
+        value: parseInt(sliderValue)
+      });
+    },
   });
-
-  const label = document.createElement('label');
-  label.for = 'contextDistance';
-  label.innerHTML = ' ' + model.contextDistance + ' Context Distance';
-
-  control.appendChild(input);
-  control.appendChild(label);
-
-  return control;
 };
