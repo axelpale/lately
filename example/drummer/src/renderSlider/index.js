affd114be30502fb81de63b1d122b943e91b122d
Akseli Palen <akseli.palen@gmail.com>
2019-05-24 01:32:51 +0300

    renderSlider

---
+++ example/drummer/src/renderSlider/index.js
@@ -0,0 +1,31 @@
+module.exports = (opts) => {
+  const control = document.createElement('div');
+  control.classList.add(opts.class);
+
+  const label = document.createElement('label');
+  label.classList.add('frame-label');
+  label.for = opts.name;
+  label.innerHTML = opts.label;
+  control.appendChild(label);
+
+  const input = document.createElement('input');
+  input.type = 'range';
+  input.value = opts.value;
+  input.name = opts.name;
+  input.min = '1';
+  input.max = '16';
+  input.step = '1';
+
+  input.addEventListener('change', () => {
+    opts.change(parseInt(input.value));
+  });
+
+  control.appendChild(input);
+
+  const postlabel = document.createElement('label');
+  postlabel.for = opts.name;
+  postlabel.innerHTML = ' ' + opts.value;
+  control.appendChild(postlabel);
+
+  return control;
+};
