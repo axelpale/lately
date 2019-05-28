dd87b3a28d1eb082ad4f6b89c3db2f6e23f88548
Akseli Palen <akseli.palen@gmail.com>
2019-05-29 01:49:38 +0300

    Fix averageContext edge bias

--- example/drummer/local_modules/mcbsp/pattern.js
+++ example/drummer/local_modules/mcbsp/pattern.js
@@ -23,13 +23,20 @@ exports.averageContext = (history, values, mask) => {
   // We add up the matching parts of the history to see what
   // happens around the pattern. Let us init the sum with zeros.
   let sliceSum = way.map(mask, q => 0)
-  let supportSum = 0
+  let supportSum = way.map(mask, q => 0)
 
   for (let t = -len; t < hlen; t += 1) {
     // Take a slice of history at time t.
     let tslice = way.slice(history, Math.max(0, t), t + len)
+    let tsliceLen = way.len(tslice)
+
     // A mask for this t.
     let tmask = mask
+
+    // To deal with the edges: a mask for support;
+    // did cell participate to to sliceSum.
+    // Cell-wise normalisation.
+    supportMask = way.map(mask, q => 1)
 
     // We do not want bias near the edges.
     // Deal with the edges by padding the slice so that size matches the mask.
@@ -37,6 +44,7 @@ exports.averageContext = (history, values, mask) => {
     // However we do not want the padded values have any weight.
     // Therefore we mask the padded values out
     // by setting this area in mask to zero.
+    // We also keep track of normalising factor for every cell.
     if (t < 0) {
       // Pad at begin
       tslice = way.padLeft(tslice, len, 0)
@@ -45,6 +53,7 @@ exports.averageContext = (history, values, mask) => {
       //   t = -4: padLeft(slice(tmask, 4, 4), 4, 0)
       //   t = -2: padLeft(slice(tmask, 2, 4), 4, 0)
       //   t = 0: padLeft(slice(tmask, 0, 4), 4, 0)
+      supportMask = way.padLeft(way.slice(supportMask, -t, len), len, 0)
     } else if (t > hlen - len) {
       // For example, len = 4, hlen = 8:
       //   t = 4: 4 > 8 - 4 === false
@@ -57,6 +66,7 @@ exports.averageContext = (history, values, mask) => {
       //   t = 5: padRight(slice(tmask, 0, 3), 4, 0)
       //   t = 7: padRight(slice(tmask, 0, 1), 4, 0)
       //   t = 8: padRight(slice(tmask, 0, 0), 4, 0)
+      supportMask = way.padRight(way.slice(supportMask, 0, hlen - t), len, 0)
     }
 
     // Compute how much the slice resembles the pattern.
@@ -70,26 +80,27 @@ exports.averageContext = (history, values, mask) => {
     const masked = way.map2(match, tmask, (d, m) => d * m)
     // Measure how much the slice resembles the pattern
     // by summing up the matches after masking.
-    let support = way.reduce(masked, (acc, q) => acc + q, 0)
+    let support = way.sum(masked)
+
     // If the pattern exists in the slice, support is high.
-    // To make the existence test more crispier, use only perfect matches.
-    if (support < maskMass) {
-      support = 0
+    // To make things more crispier, we use only perfect matches.
+    if (support === maskMass) {
+      // Probability sum
+      sliceSum = way.add(sliceSum, tslice)
+      // Weight sum
+      supportSum = way.add(supportSum, supportMask)
     }
-    // Lets add up the supporting slices by taking
-    // a weighted average of the slices.
-    const weightedSlice = way.scale(tslice, support)
-    sliceSum = way.add(sliceSum, weightedSlice)
-    supportSum = supportSum + support
   }
 
   // Normalise to get the weighted average.
-  // Take care of possible division by zero.
-  let sliceAverage = way.scale(sliceSum, supportSum > 0 ? 1 / supportSum : 0)
+  let sliceAverage = way.map2(sliceSum, supportSum, (sli, sup) => {
+    // Take care of possible division by zero.
+    return sup > 0 ? sli / sup : 0
+  })
 
   // If sum of support is 0, there is absolutely no matching pattern.
   // Then we just need to use our best knowledge, a priori.
-  if (supportSum === 0) {
+  if (way.sum(supportSum) === 0) {
     const prior = way.mean(history)
     // Stretch the priori
     sliceAverage = way.map(sliceAverage, (zero, c) => prior[c][0])
