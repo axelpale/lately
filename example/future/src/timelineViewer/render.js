d340427b7fb5c24370a6a04c17e28709b8cf6243
Akseli Palen <akseli.palen@gmail.com>
2019-06-04 01:18:34 +0300

    Init channel title editor

--- example/future/src/timelineViewer/render.js
+++ example/future/src/timelineViewer/render.js
@@ -1,4 +1,5 @@
 const way = require('senseway');
+const clearElem = require('../lib/clearElem');
 
 module.exports = (model, dispatch) => {
   const root = document.createElement('div');
@@ -8,21 +9,32 @@ module.exports = (model, dispatch) => {
 
   // Channel titles
   {
+    const rowInput = document.createElement('div');
+    rowInput.classList.add('row');
+    rowInput.classList.add('row-input');
+    root.appendChild(rowInput);
+
     const row = document.createElement('div');
     row.classList.add('row');
 
     const cells = document.createElement('div');
     cells.classList.add('cells');
 
-    for (let c = 0; c < W; c += 1) {
+    model.timeline.forEach((ch, c) => {
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.classList.add('cell-title');
       const val = model.channels[c].title;
       cell.innerHTML = '<div class="cell-label">' + val + '</div>';
 
+      cell.addEventListener('click', ev => {
+        clearElem(rowInput);
+        const t = model.channels[c].title;
+        rowInput.innerHTML = '<input type="text" value="' + t + '">';
+      });
+
       cells.appendChild(cell);
-    }
+    });
 
     row.appendChild(cells);
     root.appendChild(row);
