4ff51010320dd9b069ba040c2f2b6d425a0048c8
Akseli Palen <akseli.palen@gmail.com>
2019-06-06 02:34:33 +0300

    Buggy first-order prediction

--- example/future/src/lib/predict.js
+++ example/future/src/lib/predict.js
@@ -1,3 +1,4 @@
+const way = require('senseway')
 const pat = require('sensepat')
 
 module.exports = (historyValues, historyMask, channel, time) => {
@@ -20,8 +21,54 @@ module.exports = (historyValues, historyMask, channel, time) => {
 
   // Make into a sensepat
   const hist = pat.pattern(historyValues, historyMask)
+  const W = pat.width(hist)
+  const L = pat.len(hist)
 
   // 0th order prediction: channel expectance
   const prior = pat.mean(hist)
-  return prior[channel]
+  // return prior[channel]
+
+  // 1st order prediction: what does each of the neighboring cells predict?
+  // I.e. how much information each neighboring cell gives when compared
+  // to the channel mean.
+  // Find patterns
+
+  const zero = way.create(W, 5, 0)
+  const one = way.set(zero, channel, 2, 1)
+
+  const pattZero = {
+    value: zero,
+    mass: one
+  }
+  const pattOne = {
+    value: one,
+    mass: one
+  }
+
+  const pattPrior = {
+    value: way.map(zero, (q, c) => prior[c]),
+    mass: way.fill(zero, 1)
+  }
+
+  const ctx0 = pat.contextMean(hist, pattZero)
+  const ctx1 = pat.contextMean(hist, pattOne)
+
+  // Normalize masses
+  ctx0.mass = way.normalize(ctx0.mass)
+  ctx1.mass = way.normalize(ctx1.mass)
+
+  const gain0 = pat.infoGain(pattPrior, ctx0)
+  const gain1 = pat.infoGain(pattPrior, ctx1)
+
+  // Without delicate theoretical background let us treat gains
+  // as weights for now.
+  const g0 = pat.sum(gain0)
+  const g1 = pat.sum(gain1)
+
+  if (g0 + g1 === 0) {
+    return 0
+  }
+
+  // prob for 1
+  return g1 / (g0 + g1)
 }
