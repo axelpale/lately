5808d60ba4557b16fe8c539d0b78eb2d7016fcba
Akseli Palén <akseli.palen@gmail.com>
2019-06-09 02:53:38 +0300

    Render timeline with way.html

--- example/future/src/howViewer/render.js
+++ example/future/src/howViewer/render.js
@@ -6,5 +6,18 @@ module.exports = (model, dispatch) => {
 
   root.appendChild(howTitle(model, dispatch))
 
+  const timeline = document.createElement('div')
+  timeline.classList.add('how-timeline')
+
+  const wayel = document.createElement('div')
+  wayel.innerHTML = way.html(model.timeline, {
+    reversed: true,
+    heading: 'Timeline',
+    caption: 'This is the timeline - our training data set'
+  })
+  timeline.appendChild(wayel)
+
+  root.appendChild(timeline)
+
   return root
 }
