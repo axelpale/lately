dacf640b47497ef7894c2cd08f56f91f6d2c7f45
Akseli Palen <akseli.palen@gmail.com>
2019-06-11 22:24:12 +0300

    Default how.selected

--- example/future/src/howViewer/reduce.js
+++ example/future/src/howViewer/reduce.js
@@ -1,21 +1,26 @@
 const way = require('senseway');
 
 module.exports = (model, ev) => {
+  if (model.how.selected === null) {
+    model = Object.assign({}, model, {
+      how: Object.assign({}, model.how, {
+        selected: way.set(way.fill(model.timeline, 0), 0, 0, 1) // pick first
+      })
+    })
+  }
+
   switch (ev.type) {
 
     case 'HOW_EDIT_SELECTED': {
       const c = ev.channel
       const t = ev.time
-      const isAvailable = model.timeline[c][t] === null
 
-      let selected = way.fill(model.timeline, 0)
-      if (isAvailable) {
-        selected = way.set(selected, c, t, 1)
-      }
+      const zero = way.fill(model.timeline, 0)
+      const pickOne = way.set(zero, c, t, 1)
 
       return Object.assign({}, model, {
         how: Object.assign({}, model.how, {
-          selected: selected
+          selected: pickOne
         })
       })
     }
