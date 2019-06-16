b9c47da86815b852b24935cf5a4b3a734b508f15
Akseli Palén <akseli.palen@gmail.com>
2019-06-16 16:00:47 +0300

    Put event selector in howPrediction

--- example/future/src/howViewer/howPrediction/render.js
+++ example/future/src/howViewer/howPrediction/render.js
@@ -41,13 +41,36 @@ module.exports = (model, dispatch) => {
     caption: ''
   }))
 
-  root.appendChild(wayElem(predWay, {
+  // Single-cell prediction viewer & cell selector
+  const waySelected = (() => {
+    const c = model.how.select.channel
+    const t = model.how.select.time
+    return way.set(way.fill(model.timeline, 0), c, t, 1)
+  })()
+  const predElem = wayElem(predWay, {
     reversed: true,
     heading: 'Prediction',
+    selected: waySelected,
     caption: 'Context mean shows the probability of cell being 1 given '
       + 'the selected event becoming 1. With Bayes\' rule we compute '
       + 'the probability of the selected event given the context.'
-  }))
+  })
+  predElem.addEventListener('click', (ev) => {
+    const c = parseInt(ev.target.dataset.channel)
+    const t = parseInt(ev.target.dataset.time)
+
+    // NaN in case where non-cell was clicked
+    if (isNaN(c) || isNaN(t)) {
+      return
+    }
+
+    dispatch({
+      type: 'HOW_EDIT_SELECTED',
+      channel: c,
+      time: t
+    })
+  })
+  root.appendChild(predElem)
 
   return root
 }
