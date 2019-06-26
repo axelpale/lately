afde1d3d0821006fdd77332983dbc146847473f0
Akseli Palen <akseli.palen@gmail.com>
2019-06-27 01:45:50 +0300

    future.cellViewer: render list of "correlations"

--- example/future/src/cellViewer/render.js
+++ example/future/src/cellViewer/render.js
@@ -1,4 +1,5 @@
 const predict = require('../lib/predict');
+const way = require('senseway')
 
 module.exports = (model, dispatch) => {
   const root = document.createElement('div');
@@ -12,14 +13,28 @@ module.exports = (model, dispatch) => {
   const pred = predict(model, c, t);
 
   const row = document.createElement('div');
-  row.classList.add('row');
-  row.classList.add('row-input');
 
   let html = ''
   html += 'Channel ' + c + ': ' + model.channels[c].title + '<br>'
   html += 'Frame ' + t + ': ' + model.frames[t].title + '<br>'
   html += 'Probability: ' + pred.prob.toFixed(2) + '<br>'
   html += 'Channel Mean: ' + pred.priorProb.toFixed(2) + '<br>'
+  html += 'Channel Mean: ' + pred.priorProb.toFixed(2) + '<br>'
+
+  html += 'Caused by:<br>'
+  const ctxMean = (pred.prob < 0.5) ? pred.zeroMean : pred.oneMean;
+  const ctxGain = (pred.prob < 0.5) ? pred.zeroGain : pred.oneGain;
+  const cells = way.toArray(ctxGain.value).map(cell => {
+    cell.mass = ctxGain.mass[cell.channel][cell.time]
+    cell.mean = ctxMean.value[cell.channel][cell.time]
+    return cell
+  })
+  cells.sort((a, b) => b.value - a.value)
+  cells.forEach(cell => {
+    const chTitle = model.channels[cell.channel].title
+    html += chTitle + ' t' + cell.time + ' a' + cell.mean + ' '
+    html += 'g' + cell.value + ' m' + cell.mass + '<br>'
+  })
 
   row.innerHTML = html;
 
