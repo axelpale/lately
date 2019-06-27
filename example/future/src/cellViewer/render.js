6d3c080da57fa105497ce8302e9c4d850de4b178
Akseli Palen <akseli.palen@gmail.com>
2019-06-27 13:18:22 +0300

    future.cellViewer: list support for both 0 and 1

--- example/future/src/cellViewer/render.js
+++ example/future/src/cellViewer/render.js
@@ -21,26 +21,35 @@ module.exports = (model, dispatch) => {
   html += 'Channel Mean: ' + pred.priorProb.toFixed(2) + '<br>'
   html += 'Channel Mean: ' + pred.priorProb.toFixed(2) + '<br>'
 
-  html += 'Caused by:<br>'
+  const renderSupport = (ctxMean, ctxGain) => {
+    let str = ''
+    const cells = way.toArray(ctxGain.value).map(cell => {
+      cell.gain = cell.value
+      cell.mass = ctxGain.mass[cell.channel][cell.time]
+      cell.mean = ctxMean.value[cell.channel][cell.time]
+      cell.weight = cell.gain * Math.sqrt(cell.mass)
+      return cell
+    })
+    cells.sort((a, b) => b.weight - a.weight)
+    cells.forEach(cell => {
+      const chTitle = model.channels[cell.channel].title
+      str += chTitle + ' '
+      str += 't' + cell.time + ' '
+      str += 'a' + cell.mean.toFixed(3) + ' '
+      str += 'g' + cell.value.toFixed(3) + ' '
+      str += 'm' + cell.mass.toFixed(3) + ' '
+      str += 'w' + cell.weight.toFixed(3) + '<br>'
+    })
+    return str
+  }
+
   const ctxMean = (pred.prob < 0.5) ? pred.zeroMean : pred.oneMean;
   const ctxGain = (pred.prob < 0.5) ? pred.zeroGain : pred.oneGain;
-  const cells = way.toArray(ctxGain.value).map(cell => {
-    cell.gain = cell.value
-    cell.mass = ctxGain.mass[cell.channel][cell.time]
-    cell.mean = ctxMean.value[cell.channel][cell.time]
-    cell.weight = cell.gain * Math.sqrt(cell.mass)
-    return cell
-  })
-  cells.sort((a, b) => b.weight - a.weight)
-  cells.forEach(cell => {
-    const chTitle = model.channels[cell.channel].title
-    html += chTitle + ' '
-    html += 't' + cell.time + ' '
-    html += 'a' + cell.mean.toFixed(3) + ' '
-    html += 'g' + cell.value.toFixed(3) + ' '
-    html += 'm' + cell.mass.toFixed(3) + ' '
-    html += 'w' + cell.weight.toFixed(3) + '<br>'
-  })
+
+  html += '<strong>0 supported by:</strong><br>'
+  html += renderSupport(pred.zeroMean, pred.zeroGain)
+  html += '<strong>1 supported by:</strong><br>'
+  html += renderSupport(pred.oneMean, pred.oneGain)
 
   row.innerHTML = html;
 
